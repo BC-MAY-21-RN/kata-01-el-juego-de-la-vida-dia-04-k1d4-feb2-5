f9cbfe6565e069530359f18f9e9be7bfd5f719e6
jonathan <yoni_12rrs@hotmail.com>
2022-02-11 17:46:12 -0600

    Test class Board,
    Co-authored-by: Kelly: <kejoh07@gmail.com>
    Co-authored-by: ronaldo <ronaldoadan1@gmail.com>
    Co-authored-by: Gaby <gaby_5614@hotmail.com>
    Co-authored-by: Anderson  <contact@siranderss.com>

---
+++ test/board.test.js
@@ -0,0 +1,25 @@
+const Board = require('../Board');
+
+describe('Test on the class Board', () => {
+
+  const columns = 8;
+  const rows = 4;
+
+  const board = new Board(columns, rows);
+
+  test('The board array must not be empty', () => {
+    expect(board.createBoard()).not.toBeNull();
+  });
+
+  test('the board must have n columns', () => {
+    expect(board.createBoard()).toHaveLength(columns);
+  });
+
+  test('the row must have n cols', () => {
+    board.createBoard().forEach((row) => {
+      expect(row).toHaveLength(rows);
+    });
+  });
+});
+
+
