bd69968a68e7c4914bf773c158d93cbcc9f70bf1
jonathan <yoni_12rrs@hotmail.com>
2022-02-11 19:07:48 -0600

    Segunda generación v2,
    Co-authored-by: Kelly: <kejoh07@gmail.com>
    Co-authored-by: ronaldo <ronaldoadan1@gmail.com>
    Co-authored-by: Gaby <gaby_5614@hotmail.com>
    Co-authored-by: Anderson  <contact@siranderss.com>

--- Board.js
+++ Board.js
@@ -45,122 +45,122 @@ class Board {
     return printArr.join('\n');
   }
 
-/* ----------------------------------------------------- */
-checkNeighbors() {
-    let contarvecino = 0;
-    let limit_l = 0;
-    let limit_r = 0;
-    let limit_t = 0;
-    let limit_b = 0;
+  /* ----------------------------------------------------- */
+  // checkNeighbors() {
+  //     let contarvecino = 0;
+  //     let limit_l = 0;
+  //     let limit_r = 0;
+  //     let limit_t = 0;
+  //     let limit_b = 0;
 
-    const board = this.createBoard();
+  //     const board = this.createBoard();
 
-    //horizontal
-    for (let x = 0; x < this.columns; x++) 
-    {
-      //vertical
-      for (let y = 0; y < this.rows; y++) 
-      {
-        limit_l = x - 1; // [0,1] 0
-        limit_r = x + 1; // [2,1] 2
-        limit_t = y - 1; // [1,0] 0
-        limit_b = y + 1; // [1,2] 2
-        if (limit_l < 0) 
-        {
-          limit_l = -1;
-        }
-        if (limit_r < 0) {
-          limit_r = -1;
-        }
-        if (limit_t < 0) {
-          limit_t = -1;
-        }
-        if (limit_b < 0) {
-          limit_b = -1;
-        }
+  //     //horizontal
+  //     for (let x = 0; x < this.columns; x++) 
+  //     {
+  //       //vertical
+  //       for (let y = 0; y < this.rows; y++) 
+  //       {
+  //         limit_l = x - 1; // [0,1] 0
+  //         limit_r = x + 1; // [2,1] 2
+  //         limit_t = y - 1; // [1,0] 0
+  //         limit_b = y + 1; // [1,2] 2
+  //         if (limit_l < 0) 
+  //         {
+  //           limit_l = -1;
+  //         }
+  //         if (limit_r < 0) {
+  //           limit_r = -1;
+  //         }
+  //         if (limit_t < 0) {
+  //           limit_t = -1;
+  //         }
+  //         if (limit_b < 0) {
+  //           limit_b = -1;
+  //         }
 
-        //diagonal izquierda arriba
-        if (limit_l >= 0 && limit_t >= 0) {
-          if (board[limit_l][limit_t] == '*') {
-            contarvecino = contarvecino + 1;
-          }
-        }
-        //izquierda
-        if (limit_l >= 0) {
-          if (board[limit_l][y] == '*') {
-            contarvecino = contarvecino + 1;
-          }
-        }
-        //diagonal izquierda abajo
-        if (limit_l >= 0 && limit_b >= 0) {
-          if (board[limit_l][limit_b] == '*') {
-            contarvecino = contarvecino + 1;
-          }
-        }
+  //         //diagonal izquierda arriba
+  //         if (limit_l >= 0 && limit_t >= 0) {
+  //           if (board[limit_l][limit_t] == '*') {
+  //             contarvecino = contarvecino + 1;
+  //           }
+  //         }
+  //         //izquierda
+  //         if (limit_l >= 0) {
+  //           if (board[limit_l][y] == '*') {
+  //             contarvecino = contarvecino + 1;
+  //           }
+  //         }
+  //         //diagonal izquierda abajo
+  //         if (limit_l >= 0 && limit_b >= 0) {
+  //           if (board[limit_l][limit_b] == '*') {
+  //             contarvecino = contarvecino + 1;
+  //           }
+  //         }
 
-        //arriba
-        if (limit_t >= 0) {
-          if (board[x][limit_t] == '*') {
-            contarvecino = contarvecino + 1;
-          }
-        }
+  //         //arriba
+  //         if (limit_t >= 0) {
+  //           if (board[x][limit_t] == '*') {
+  //             contarvecino = contarvecino + 1;
+  //           }
+  //         }
 
-        //abajo
-        if (limit_b >= 0) {
-          // console.log('x: ', x, 'limit_b: ', limit_b);
-          // console.log('matrix: ', board[x][limit_b]);
-          if (board[x][limit_b] == ' * ') {
-            contarvecino = contarvecino + 1;
-          }
-        }
+  //         //abajo
+  //         if (limit_b >= 0) {
+  //           // console.log('x: ', x, 'limit_b: ', limit_b);
+  //           // console.log('matrix: ', board[x][limit_b]);
+  //           if (board[x][limit_b] == ' * ') {
+  //             contarvecino = contarvecino + 1;
+  //           }
+  //         }
 
-        //diagonal derecha arriba
-        if (limit_r >= 0 && limit_r < this.columns && limit_t >= 0) {
-          //console.log('x: ', limit_r);
-          //console.log('y: ', limit_t);
-          if (board[limit_r][limit_t] == '*') {
-            contarvecino = contarvecino + 1;
-          }
-        }
-        //derecha
-        if (limit_r >= 0 && limit_r < this.columns) {
-          //console.log('x: ', limit_r);
-          //console.log('y: ', y);
-          if (board[limit_r][y] == '*') {
-            contarvecino = contarvecino + 1;
-          }
-        }
-        //diagonal derecha abajo
-        if (
-          limit_r >= 0 &&
-          limit_r <= this.columns &&
-          limit_b >= 0 &&
-          limit_b <= this.rows
-        ) {
-          //   if (this.matrix[limit_r][limit_b] == '*') {
-          //     contarvecino = contarvecino + 1;
-          //   }
-        }
+  //         //diagonal derecha arriba
+  //         if (limit_r >= 0 && limit_r < this.columns && limit_t >= 0) {
+  //           //console.log('x: ', limit_r);
+  //           //console.log('y: ', limit_t);
+  //           if (board[limit_r][limit_t] == '*') {
+  //             contarvecino = contarvecino + 1;
+  //           }
+  //         }
+  //         //derecha
+  //         if (limit_r >= 0 && limit_r < this.columns) {
+  //           //console.log('x: ', limit_r);
+  //           //console.log('y: ', y);
+  //           if (board[limit_r][y] == '*') {
+  //             contarvecino = contarvecino + 1;
+  //           }
+  //         }
+  //         //diagonal derecha abajo
+  //         if (
+  //           limit_r >= 0 &&
+  //           limit_r <= this.columns &&
+  //           limit_b >= 0 &&
+  //           limit_b <= this.rows
+  //         ) {
+  //           //   if (this.matrix[limit_r][limit_b] == '*') {
+  //           //     contarvecino = contarvecino + 1;
+  //           //   }
+  //         }
 
-        // 1. Cualquier célula viva con menos de dos vecinas vivas muere, como si la causa fuera la subpoblación.
-        // 3. Cualquier celda viva con dos o tres vecinos vivos vive en la próxima generación.
-        if (board[x][y] == '*') {
-          if (contarvecino == 2 || contarvecino == 3) {
-            board[x][y] = '*';
-          } else {
-            board[x][y] = '.';
-          }
-        } else {
-          if (contarvecino == 3) {
-            board[x][y] = '*';
-          }
-        }
-        console.log(board[x][y]);
-      }
-      console.log('--');
-    }
-  }
-/* ----------------------------------------------------- */
+  //         // 1. Cualquier célula viva con menos de dos vecinas vivas muere, como si la causa fuera la subpoblación.
+  //         // 3. Cualquier celda viva con dos o tres vecinos vivos vive en la próxima generación.
+  //         if (board[x][y] == '*') {
+  //           if (contarvecino == 2 || contarvecino == 3) {
+  //             board[x][y] = '*';
+  //           } else {
+  //             board[x][y] = '.';
+  //           }
+  //         } else {
+  //           if (contarvecino == 3) {
+  //             board[x][y] = '*';
+  //           }
+  //         }
+  //         console.log(board[x][y]);
+  //       }
+  //       console.log('--');
+  //     }
+  //   }
+  /* ----------------------------------------------------- */
 }
 
 module.exports = Board;
